07ade35b2263e99ea17265abe4345bc564d8e5cf
Yana <yanalapitskay@gmail.com>
2017-12-19 01:51:52 +0100

    added header which allow response to be exposed to the page

--- app.js
+++ app.js
@@ -27,12 +27,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
