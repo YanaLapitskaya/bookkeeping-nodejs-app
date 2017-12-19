fa122f40847ca62c8b7ecde77cead6c637918f76
Yana <yanalapitskay@gmail.com>
2017-12-19 01:58:43 +0100

    completely rewrited transaction controller

--- api/controllers/transaction.js
+++ api/controllers/transaction.js
@@ -5,25 +5,28 @@ const User = require('../models/User');
  * GET /all
  * Returns all transactions of the current user.
  */
-exports.getTransactions = (req,res,next)=>{
-	User.findById(req.user.id, (err, user) => {
-		if (err) { return next(err); }
+exports.getTransactions = (req,res,next)=> {
+	User.findById(req.user.id)
+		.populate('transactions')
+		.exec((err, user) => {
+			if (err) return next(err);
 
-		return res.status(200).send({transactions: user.transactions});
-	});
+			res.status(200).send({transactions: user.transactions});
+		});
 };
-
 
 /**
  * GET /
  * Returns the transaction with id.
  */
 exports.getTransaction = (req,res,next)=>{
-	Transaction.findById(req.params.id, (err, trans) => {
-		if (err) { return next(err); }
+	User.findById(req.user.id)
+		.populate({path: 'transactions', match: {_id: req.params.id}})
+		.exec((err, user) => {
+			if (err) return next(err);
 
-		return res.status(200).send({transaction: trans});
-	});
+			res.status(200).send({transaction: user.transactions[0]});
+		});
 };
 
 /**
@@ -36,19 +39,19 @@ exports.addTransaction = (req,res,next)=>{
 		amount: req.body.amount,
 		type: req.body.type
 	});
+	trans.save(function (err) {
+		if (err) return next(err);
+	});
 
-	trans.save((err)=>{
-		if(err) {return next(err);}
+	User.findById(req.user.id, (err, user) => {
+		if (err) { return next(err); }
 
-		User.findById(req.user.id, (err, user) => {
-			if (err) { return next(err); }
+		user.transactions.push(trans._id);
+		user.save((err) => {
+			if (err) return next(err);
 
-			user.transactions.push(trans);
-			user.save((err) => {
-				if (err) return next(err);
-			});
+			res.status(200).send({transaction: trans});
 		});
-		return res.status(200).send({transaction: trans});
 	});
 };
 
@@ -58,16 +61,17 @@ exports.addTransaction = (req,res,next)=>{
  */
 exports.editTransaction = (req,res,next)=>{
 	Transaction.findById(req.params.id, (err, trans) => {
-		if (err) { return next(err); }
+		if (err) return next(err);
 
 		trans.title = req.body.title || '';
 		trans.amount = req.body.amount || '';
 		trans.type = req.body.type || '';
 		//trans.card = req.body.card || '';
 		//trans.check = req.body.check || '';
-		trans.details = req.body.location || '';
+
 		trans.save((err) => {
 			if (err) return next(err);
+
 			return res.status(200).send({ msg: 'Transaction has been updated.', transaction: trans });
 		});
 	});
